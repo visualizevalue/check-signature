255716da24d6fe9f4353849fbda9d68c51906f4c
jalil <jalil@1001.digital>
2023-12-05 21:23:12 -0500

    Add pwa config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,6 +49,42 @@ export default defineNuxtConfig({
       ],
     }
   },
+  pwa: {
+    /* PWA options */
+    devOptions: {
+      enabled: true,
+    },
+    manifest: {
+      name: 'Signature Check',
+      short_name: 'Sign',
+      description: 'Create and verify statements created with Ethereum Accounts.',
+      theme_color: '#000000',
+      icons: [
+        {
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ],
+    },
+  },
   runtimeConfig: {
     public: {
       api: '',
